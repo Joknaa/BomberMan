38b5a2138d60b321f1e72b4d9846a6b1d619594d
Oknaa <mohammadlaadidaoui@gmail.com>
2022-09-06 19:00:37 +0100

    Tweaked the leaderboard colors and design

--- BomberMan/Assets/_Scripts/Units/Player.cs
+++ BomberMan/Assets/_Scripts/Units/Player.cs
@@ -1,5 +1,5 @@
-﻿
-using System;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,20 +8,30 @@ namespace BomberMan {
         public LayerMask MovementStopper;
         public Transform DestinationPoint;
         public int moveSpeed;
+        // public float moveDuration;
         public int bombCount;
         public int bombRange;
         public int bombTimer;
-        
+
         private float _detectionCircleRadius = 0.2f;
         private bool _isDead;
+        private bool _isMoving;
 
         private void Start() {
             DestinationPoint.parent = null;
         }
 
         private void Update() {
-            if(GameStateController.Instance.GetState() != GameState.Playing) return;
+            if (GameStateController.Instance.GetState() != GameState.Playing) return;
+            /*var horizontalInput = Input.GetAxisRaw("Horizontal");
+            var verticalInput = Input.GetAxisRaw("Vertical");
+            print("horizontalInput: " + horizontalInput + " verticalInput: " + verticalInput);
+            if (verticalInput > 0 && !_isMoving && !IsSomethingAhead(Vector2.up)) StartCoroutine(Move(Vector2.up));
+            if (verticalInput < 0 && !_isMoving && !IsSomethingAhead(Vector2.down)) StartCoroutine(Move(Vector2.down));
+            if (horizontalInput < 0 && !_isMoving && !IsSomethingAhead(Vector2.left)) StartCoroutine(Move(Vector2.left));
+            if (horizontalInput > 0 && !_isMoving && !IsSomethingAhead(Vector2.right)) StartCoroutine(Move(Vector2.right));*/
             
+
             var distanceToDestination = Vector2.Distance(transform.position, DestinationPoint.position);
             if (distanceToDestination <= 0.05f) {
                 var horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -29,7 +39,7 @@ namespace BomberMan {
                 var verticalInput = Input.GetAxisRaw("Vertical");
                 var verticalDirection = new Vector3(0, verticalInput, 0);
                 
-                if (Mathf.Abs(horizontalInput) == 1 ) {
+                if (Mathf.Abs(horizontalInput) == 1) {
                     if (!IsSomethingAhead(horizontalDirection)) {
                         DestinationPoint.position += horizontalDirection;
                     }
@@ -39,30 +49,48 @@ namespace BomberMan {
                     }
                 }
             }
-            
+
             if (Input.GetKeyDown(KeyCode.Space) && bombCount > 0) {
                 PlaceBomb();
                 bombCount--;
             }
-            
+
             bool IsSomethingAhead(Vector3 direction) {
                 return Physics2D.OverlapCircle(DestinationPoint.position + direction, _detectionCircleRadius, MovementStopper);
             }
         }
-        
+
         private void FixedUpdate() {
-            if(GameStateController.Instance.GetState() != GameState.Playing) return;
+            if (GameStateController.Instance.GetState() != GameState.Playing) return;
             transform.position = Vector3.MoveTowards(transform.position, DestinationPoint.position, moveSpeed * Time.deltaTime);
         }
-        
+
+        /*
+        private IEnumerator Move(Vector2 moveDirection) {
+            _isMoving = true;
+            var elapsedTime = 0f;
+            var originalPosition = transform.position;
+            DestinationPoint.position += (Vector3) moveDirection;
+            var targetPosition = DestinationPoint.position + (Vector3)moveDirection;
+ 
+            while (elapsedTime < moveDuration) {
+                transform.position = Vector2.Lerp(originalPosition, DestinationPoint.position, elapsedTime / moveDuration);
+                elapsedTime += Time.deltaTime;
+                yield return null; 
+            }
+            transform.position = DestinationPoint.position;
+            _isMoving = false;
+        }
+        */
+
         private void PlaceBomb() {
-            if(GameStateController.Instance.GetState() != GameState.Playing) return;
+            if (GameStateController.Instance.GetState() != GameState.Playing) return;
             _gridManager.PlaceBomb(transform.position, bombRange, bombTimer);
         }
 
         public void OnDeath() {
             _isDead = true;
-            if(GameStateController.Instance.GetState() != GameState.Playing) return;
+            if (GameStateController.Instance.GetState() != GameState.Playing) return;
             GameStateController.Instance.SetState(GameState.GameOver);
             Destroy(gameObject);
         }
